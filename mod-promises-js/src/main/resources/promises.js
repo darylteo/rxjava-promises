20a01a8dec6b36ad6b651511cb699f93744610e1
Daryl Teo <i.am@darylteo.com>
2013-03-13 15:31:40 +1100

    Cleanup of promises.js

--- mod-promises-js/src/main/resources/promises.js
+++ mod-promises-js/src/main/resources/promises.js
@@ -1,5 +1,5 @@
-module.exports = {
-  "defer" : com.darylteo.promises.Promise.defer
-}
-
+// Register the Javascript Language Adaptor for RxJava
 com.darylteo.promises.js.JavascriptAdaptor.registerLanguageAdaptor();
+
+// Export the Promise class, so that static methods can be called
+module.exports = com.darylteo.promises.Promise
